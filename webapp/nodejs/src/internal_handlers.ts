cb59a990ff4e373e70427d17aeb09a72bbb10bfe
ningen <ningen361@gmail.com>
2024-12-08 17:07:01 +0900

    interanl の更新（動かない）

--- webapp/nodejs/src/internal_handlers.ts
+++ webapp/nodejs/src/internal_handlers.ts
@@ -6,39 +6,57 @@ import type { Chair, Ride } from "./types/models.js";
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 export const internalGetMatching = async (ctx: Context<Environment>) => {
   // MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
-  const [[ride]] = await ctx.var.dbConn.query<Array<Ride & RowDataPacket>>(
-    "SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1",
-  );
-  if (!ride) {
-    return ctx.body(null, 204);
-  }
-  let matched!: Chair & RowDataPacket;
-  let empty = false;
-  for (let i = 0; i < 10; i++) {
-    [[matched]] = await ctx.var.dbConn.query<Array<Chair & RowDataPacket>>(
-      "SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1",
+  try {
+    await ctx.var.dbConn.beginTransaction();
+
+    const [[ride]] = await ctx.var.dbConn.query<Array<Ride & RowDataPacket>>(
+      "SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1",
     );
-    if (!matched) {
+    if (!ride) {
       return ctx.body(null, 204);
     }
-    const [[result]] = await ctx.var.dbConn.query<
-      Array<{ "COUNT(*) = 0": number } & RowDataPacket>
-    >(
-      "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE",
-      [matched.id],
+    const [chairs] = await ctx.var.dbConn.query<Array<Chair & RowDataPacket>>(
+      `
+        SELECT
+          c.*
+        FROM
+          chairs c
+        WHERE
+          c.is_active = TRUE
+          AND NOT EXISTS (
+            SELECT
+              1
+            FROM
+              rides r
+              INNER JOIN latest_ride_statuses rs ON r.id = rs.ride_id
+            WHERE
+              r.chair_id = c.id
+              AND rs.status <> 'COMPLETED'
+          )
+        ORDER BY
+          RAND()
+        LIMIT 1
+      `,
+      [ride.pickup_latitude, ride.pickup_longitude],
     );
-    empty = !!result["COUNT(*) = 0"];
-    if (empty) {
-      break;
+
+    console.log("=================== chairs =========================")
+    console.log(chairs)
+    console.log("===============================================")
+    if (!!chairs.length) {
+      await ctx.var.dbConn.query("UPDATE rides SET chair_id = ? WHERE id = ?", [
+        chairs[0].id,
+        ride.id,
+      ]);
     }
-  }
-  if (!empty) {
+
+    await ctx.var.dbConn.commit();
+  } catch (e) {
+    await ctx.var.dbConn.rollback();
+    console.error("===============================================")
+    console.error(e)
+    console.error("===============================================")
+  } finally {
     return ctx.body(null, 204);
   }
-  await ctx.var.dbConn.query("UPDATE rides SET chair_id = ? WHERE id = ?", [
-    matched.id,
-    ride.id,
-  ]);
-
-  return ctx.body(null, 204);
 };
