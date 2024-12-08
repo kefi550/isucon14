13eb747c89974b1a7ec817a212511be20f1dc5be
kefi550 <ytiryrstd550@gmail.com>
2024-12-08 10:10:34 +0900

    もろもろpull.sh

--- pull.sh
+++ pull.sh
@@ -7,3 +7,16 @@ LOCAL_PATH="."
 mkdir -p ${LOCAL_PATH}/webapp/nodejs/
 rsync -avr isu1:~/webapp/nodejs/ "${LOCAL_PATH}/webapp/nodejs/"
 
+for i in $(seq 1 3); do
+  host="isu${i}"
+  mkdir -p ${LOCAL_PATH}/${host}/etc/systemd/system/
+  rsync -avr ${host}:/etc/hosts "${LOCAL_PATH}/${host}/etc/"
+  rsync -avr ${host}:/etc/systemd/system/isuports.service "${LOCAL_PATH}/${host}/etc/systemd/system/"
+  mkdir -p ${LOCAL_PATH}/${host}/etc/mysql/
+  rsync -avr ${host}:/etc/mysql/ "${LOCAL_PATH}/${host}/etc/mysql/"
+  mkdir -p ${LOCAL_PATH}/${host}/etc/nginx/
+  rsync -avr ${host}:/etc/nginx/ "${LOCAL_PATH}/${host}/etc/nginx/"
+  mkdir -p ${LOCAL_PATH}/${host}/etc/logrotate.d/
+  rsync -avr ${host}:/etc/logrotate.d/ "${LOCAL_PATH}/${host}/etc/logrotate.d/"
+done
+
